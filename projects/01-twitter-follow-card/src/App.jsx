d037f61afbbe9fdfef7d0c52a7c30a6fdc9bb57a
ARM-developer <angel_alexito123@hotmail.com>
2023-09-30 19:28:58 -0600

    first commit

---
+++ projects/01-twitter-follow-card/src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css'
+import { TwitterFollowCard } from './components/TwitterFollowCard'
+import { users } from './utils/constants.js'
+
+export function App () {
+  return (
+    <section className='App'>
+      {
+        users.map(({ userName, name, isFollowing }) => (
+          <TwitterFollowCard
+            key={userName}
+            userName={userName}
+            initialIsFollowing={isFollowing}
+          >
+            {name}
+          </TwitterFollowCard>
+        ))
+      }
+    </section>
+  )
+}
